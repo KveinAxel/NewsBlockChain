1ec9b55daf32433cad00bfa3296158b2ace9e5dd
Kevin Axel <2438679008@qq.com>
2020-03-31 09:30:14 +0800

    add logger example

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include "util/Logger.h"
+LOGGER::CLogger logger(LOGGER::LogLevel_Info, LOGGER::CLogger::GetAppAbsolutePath().append("log\\"));
 
 int main(int argc, char *argv[])
 {
     std::cout << "news block chain" << std::endl;
+    logger.traceInfo("News Block Chain Inited");
     return 0;
 }
