f4fca23ed4676e94a3f0ea4ae6317b3fa234316e
Kevin Axel <2438679008@qq.com>
2020-04-02 00:08:13 +0800

    add unit test

--- test/compressor/TestCompressor.cpp
+++ test/compressor/TestCompressor.cpp
@@ -1,2 +1,27 @@
-#include "gtest.h"
-TEST()
+#include "gtest/gtest.h"
+#include "service/compressor/Compressor.h"
+
+// 测试还原性
+TEST(TestCompressor, testReverse) {
+
+    // 获得压缩器
+    std::string data("test the compressor");
+    Compressor compressor(data);
+    compressor.compress();
+
+    // 压缩成功
+    ASSERT_TRUE(compressor.isCompleted());
+    std::string serializedDict = compressor.getSerializedDict();
+    std::vector<uint32_t> bytes = compressor.getBytes();
+    uint32_t filler = compressor.getFiller();
+
+    // 根据压缩信息构造解压器
+    Compressor decompressor(serializedDict, bytes, filler);
+    decompressor.decompress();
+
+    // 解压成功
+    ASSERT_TRUE(decompressor.isCompleted());
+
+    // 信息还原
+    ASSERT_EQ(data, decompressor.getData());
+}
