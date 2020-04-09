926d3aa03ab26329fbb5c729584b209b2aa2f27b
XuanLaoYee <1115810634@qq.com>
2020-04-09 20:25:14 +0800

    Change MerkelTree to static class

--- src/util/MerkleTree.cpp
+++ src/util/MerkleTree.cpp
@@ -2,11 +2,7 @@
 #include "Hash.h"
 #include "QVector"
 #include "QQueue"
-MerkleTree::~MerkleTree() {
-    //delete[] merkleIndex;
-}
-QList<int> MerkleTree::modifyCheck(QList<std::string> article) {
-    int ID = 0;
+QList<int> MerkleTree::modifyCheck(QList<std::string> article,QVector<std::string>* merkleIndex) {
     int paraNum = article.size();
     QVector<TreeNode> nodeList;
     for(int i=0;i<paraNum;i++){
@@ -14,10 +10,8 @@ QList<int> MerkleTree::modifyCheck(QList<std::string> article) {
 
         TreeNode newTreeNode;
         newTreeNode.hash = hashCode.toString();
-        newTreeNode.id = ID++;
         newTreeNode.left = -1;
         newTreeNode.right = -1;
-        newTreeNode.para = i+1;
 
         nodeList.push_back(newTreeNode);
     }
@@ -25,36 +19,32 @@ QList<int> MerkleTree::modifyCheck(QList<std::string> article) {
         TreeNode newTreeNode;
         Hash hashCode(nodeList.at(i).hash+nodeList.at(i+1).hash);
         newTreeNode.hash = hashCode.toString();
-        newTreeNode.id = ID++;
-        newTreeNode.left = nodeList.at(i).id;
-        newTreeNode.right = nodeList.at(i+1).id;//这样改高效一点，以后编程也要尽量这样，时间上也差不多，应为文章段数比较小
-        newTreeNode.para = -1;
+        newTreeNode.left = i;
+        newTreeNode.right = i+1;//这样改高效一点，以后编程也要尽量这样，时间上也差不多，应为文章段数比较小
         nodeList.push_back(newTreeNode);
-        if(nodeList.size()==i+2){
+        if(nodeList.size() < i+2){
             break;
         }
     }
     QList<int> output;
-    if(nodeList.last().hash == this->nodeList.last().hash){
+    if(nodeList.last().hash == merkleIndex->last()){
         return output;
     } else{
         QQueue<TreeNode> q;
-        if(nodeList.at(nodeList.last().left).hash!=this->nodeList.at(nodeList.last().left).hash)
-            q.push_back(nodeList.at(nodeList.last().left));
-        if(nodeList.at(nodeList.last().right).hash!=this->nodeList.at(nodeList.last().right).hash)
+        q.push_back(nodeList[paraNum-1]);
         do{
             TreeNode nextSearch = q.front();
             q.pop_front();
-            if(nodeList[nextSearch.left].hash != this->nodeList[nextSearch.left].hash){
-                if(nodeList[nextSearch.left].para != -1){
-                    output.append(nodeList[nextSearch.left].para);
+            if(nodeList[nextSearch.left].hash != merkleIndex->at(nextSearch.left)){
+                if(nextSearch.left < article.size()){
+                    output.append(nextSearch.left+1);
                 }else{
                     q.push_back(nodeList[nextSearch.left]);
                 }
             }
-            if(nodeList[nextSearch.right].hash != this->nodeList[nextSearch.right].hash){
-                if(nodeList[nextSearch.right].para != -1){
-                    output.append(nodeList[nextSearch.right].para);
+            if(nodeList[nextSearch.right].hash != merkleIndex->at(nextSearch.right)){
+                if(nextSearch.right < article.size()){
+                    output.append(nextSearch.right+1);
                 }else{
                     q.push_back(nodeList[nextSearch.right]);
                 }
@@ -63,37 +53,34 @@ QList<int> MerkleTree::modifyCheck(QList<std::string> article) {
     }
     return output;
 }
-MerkleTree::MerkleTree(const QList<std::string> article){
-    int ID = 0;
+QVector<std::string>* MerkleTree::getMerkleArray(QList<std::string> article){
     int paraNum = article.size();
+    QVector<TreeNode> nodeList;
     for(int i=0;i<paraNum;i++){
         Hash hashCode(article.at(i));
 
         TreeNode newTreeNode;
         newTreeNode.hash = hashCode.toString();
-        newTreeNode.id = ID++;
 
         nodeList.push_back(newTreeNode);
-
     }
     for(int i=0;i<nodeList.size();i+=2){
         TreeNode newTreeNode;
         Hash hashCode(nodeList.at(i).hash+nodeList.at(i+1).hash);
         newTreeNode.hash = hashCode.toString();
-        newTreeNode.id = ID++;
-        newTreeNode.left = nodeList.at(i).id;
-        newTreeNode.right = nodeList.at(i+1).id;//这样改高效一点，以后编程也要尽量这样，时间上也差不多，应为文章段数比较小
-        newTreeNode.para = -1;
+        newTreeNode.left = i;
+        newTreeNode.right = i+1;//这样改高效一点，以后编程也要尽量这样，时间上也差不多，应为文章段数比较小
         nodeList.push_back(newTreeNode);
-        if(nodeList.size()==i+2){
+        if(nodeList.size() < i+2){
             break;
         }
     }
-//    merkleIndex = new string[nodeList.size()];
-//    for(int i=0;i<nodeList.size();i++){
-//        merkleIndex[i] = nodeList.at(i).hash;
-//    }
-    //return ID;
+    QVector<std::string>* merkleIndex;
+    merkleIndex = new QVector<std::string>;
+    for(int i=0;i<nodeList.size();i++){
+        merkleIndex->push_back(nodeList.at(i).hash);
+    }
+    return merkleIndex;
 }
 
 //std::string * MerkleTree::getMerkleArray() {
