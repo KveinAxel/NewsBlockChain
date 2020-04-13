2b1c0ea41815e85b270def2f262da409def36450
Kevin Axel <2438679008@qq.com>
2020-04-13 14:04:17 +0800

    merge article generator

---
+++ src/util/BullShitArticleGenerator.h
@@ -0,0 +1,12 @@
+#ifndef NEWS_BLOCK_CHAIN_BULL_SHIT_ARTICLE_GENERATOR_H
+#define NEWS_BLOCK_CHAIN_BULL_SHIT_ARTICLE_GENERATOR_H
+#include <vector>
+#include <string>
+
+class BullShitArticleGenerator {
+public:
+    static std::string generator();
+};
+
+
+#endif // NEWS_BLOCK_CHAIN_BULL_SHIT_ARTICLE_GENERATOR_H
