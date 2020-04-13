3884f0747710c4de7233053335cd2113c1bf5a08
XuanLaoYee <1115810634@qq.com>
2020-04-13 10:15:46 +0800

    Add BullShitArticleGenerator

---
+++ src/util/BullShitArticleGenerator.h
@@ -0,0 +1,12 @@
+#ifndef NEWSBLOCKCHAIN_BULLSHITARTICLEGENERATOR_H
+#define NEWSBLOCKCHAIN_BULLSHITARTICLEGENERATOR_H
+#include "QList"
+#include "string"
+
+class BullShitArticleGenerator {
+public:
+    static std::string generator();
+};
+
+
+#endif //NEWSBLOCKCHAIN_BULLSHITARTICLEGENERATOR_H
